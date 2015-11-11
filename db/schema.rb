4624f02f2aef519aa533268a16ddbc3cc7d2c68a
= <=>
2015-11-11 08:08:07 -0800

    Add column quiestion_id to answer

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151111153655) do
+ActiveRecord::Schema.define(version: 20151111160436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "answers", force: :cascade do |t|
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "question_id"
   end
 
   create_table "questions", force: :cascade do |t|
