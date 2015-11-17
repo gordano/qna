f248ac703ccaa322e8db8a9a5353b989ff44cd34
= <=>
2015-11-17 11:46:10 -0800

    Add spec #edit

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,21 @@
 class QuestionsController < ApplicationController
+	before_action :find_question, only: [:show, :edit]
 	def index
       @questions = Question.all
 	end
 
-	def show
-      @question = Question.find(params[:id])
+	def show      
 	end
 
 	def new 
       @question = Question.new
 	end
+
+	def edit     
+	end
+
+	private
+			def find_question
+			  @question = Question.find(params[:id])
+			end 
 end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -32,4 +32,14 @@ RSpec.describe QuestionsController, :type => :controller do
         expect(response).to render_template :new
       end
     end
+
+    describe 'GET #edit' do      	
+      before { get :edit, id: question } 
+      it 'assigns the requested question to @question' do
+        expect(assigns(:question)).to eq question
+      end
+      it 'render edit view' do
+        expect(response).to render_template :edit
+      end
+    end
 end
