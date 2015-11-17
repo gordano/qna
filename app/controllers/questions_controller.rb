dc359dd086a6c87e8873875ba0ead6c9f8d4f3a2
= <=>
2015-11-17 13:16:21 -0800

    spec Question add CREATE PATCH DELETE

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-	before_action :find_question, only: [:show, :edit]
+	before_action :find_question, only: [:show, :edit, :update, :destroy]
 	def index
       @questions = Question.all
 	end
@@ -14,8 +14,33 @@ class QuestionsController < ApplicationController
 	def edit     
 	end
 
+	def create
+	  @question = Question.new(question_params)	  	
+      if @question.save
+      	redirect_to question_path(@question)
+      else
+      	render :new
+      end
+	end
+
+	def update
+      if @question.update(question_params)      	
+        redirect_to @question
+      else
+      	render :edit
+      end
+	end
+
+	def destroy
+      @question.destroy
+      redirect_to questions_path
+	end
+
 	private
 			def find_question
 			  @question = Question.find(params[:id])
+			end
+			def question_params
+              params.require(:question).permit(:title, :body)
 			end 
 end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -42,4 +42,67 @@ RSpec.describe QuestionsController, :type => :controller do
         expect(response).to render_template :edit
       end
     end
+
+    describe 'POST #create' do
+      context 'with valid attributes' do
+        it 'saved the new question in database' do
+          expect { post :create, question: attributes_for(:question) }.to change(Question, :count).by(1)
+        end
+        it 'redirected to  show view' do
+          post :create, question: attributes_for(:question)
+          expect(response).to redirect_to question_path(assigns(:question))
+        end
+      end
+      context 'with invalid attributes' do
+        it 'not saved new question in database' do
+          expect { post :create, question: attributes_for(:invalid_question) }.to_not change(Question, :count)
+        end
+
+        it 're-renders new view' do 
+          post :create, question: attributes_for(:invalid_question)
+          expect(response).to render_template :new
+        end	
+      end
+      
+    end
+    describe 'PATCH #update' do
+      context 'valid attributes' do
+      	it 'assigns the requested question to @question' do
+          patch :update, id: question, question: attributes_for(:question)
+          expect(assigns(:question)).to eq question
+      	end
+      	it 'change question attributes' do
+          patch :update, id: question, question: {title: 'new title', body: 'new body'}
+          question.reload
+          expect(question.title).to eq 'new title'
+          expect(question.body).to eq 'new body'
+      	end
+      	it 'redirected to updated question' do
+          patch :update, id: question, question: attributes_for(:question)
+          expect(response).to redirect_to question
+        end
+      end
+      context 'invalid attributes' do
+      	before {patch :update, id: question, question: {title: 'new title', body: nil}}
+      	it 'not change question attributes' do         
+          question.reload
+          #expect(question.title).to eq 'new title'
+          #expect(question.body).to eq 'new body'
+          expect(assigns(:question)).to eq question
+      	end
+      	it 're-renders edit view' do
+          expect(response).to render_template :edit
+      	end
+      end
+    end
+    describe 'DELETE #destroy' do
+      before { question }
+      it 'deleted question' do        	
+        expect { delete :destroy, id: question}.to change(Question, :count).by(-1)
+      end
+      it 'redirect to index view' do
+        delete :destroy, id: question
+        expect(response).to redirect_to questions_path
+      end
+    end
 end
