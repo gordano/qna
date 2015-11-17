92d1b0fff84ae9207a655250fc59bafe8a6969bf
= <=>
2015-11-17 11:25:14 -0800

    Add spec #show

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,17 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, :type => :controller do
-	describe 'GET #index' do
-	  before do
-	  	@questions = FactoryGirl.create_list(:question, 2)
+	let(:questions) {create_list(:question, 2)}
+	describe 'GET #index' do	  
+	  before do	  	
 	  	get :index
 	  end	
       it 'populates an array of all questions' do
-        expect(assigns(:questions)).to match_array(@questions)
+        expect(assigns(:questions)).to match_array(questions)
       end
       it 'renders index view' do
       	expect(response).to render_template :index
       end
 	end
-
+    
+    describe 'GET #show' do
+      let(:question) {create(:question)}	
+      before do
+       get :show, id: question
+      end 
+      it 'assigns the requested question to @question' do
+        expect(assigns(:question)).to eq question
+      end
+      it 'render show view' do
+        expect(response).to render_template :show
+      end
+    end
 end
