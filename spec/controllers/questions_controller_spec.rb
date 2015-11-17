c43ecb5c3ffef3b2fee5a6fa6471223b328653d6
= <=>
2015-11-17 11:36:39 -0800

    Add spec #new

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -2,10 +2,9 @@ require 'rails_helper'
 
 RSpec.describe QuestionsController, :type => :controller do
 	let(:questions) {create_list(:question, 2)}
+	let(:question) {create(:question)}
 	describe 'GET #index' do	  
-	  before do	  	
-	  	get :index
-	  end	
+	  before { get :index }	  	
       it 'populates an array of all questions' do
         expect(assigns(:questions)).to match_array(questions)
       end
@@ -14,16 +13,23 @@ RSpec.describe QuestionsController, :type => :controller do
       end
 	end
     
-    describe 'GET #show' do
-      let(:question) {create(:question)}	
-      before do
-       get :show, id: question
-      end 
+    describe 'GET #show' do      	
+      before { get :show, id: question } 
       it 'assigns the requested question to @question' do
         expect(assigns(:question)).to eq question
       end
       it 'render show view' do
         expect(response).to render_template :show
+      end
+    end
+
+    describe 'GET #new' do
+      before { get :new }
+      it 'assigns a new Question to @question' do
+        expect(assigns(:question)).to be_a_new(Question)
+      end
+      it 'render new view' do
+        expect(response).to render_template :new
       end
     end
 end
