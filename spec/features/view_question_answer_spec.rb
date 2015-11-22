7f572fa3ce7c64342338ea10d0f3b7773cfb321e
= <=>
2015-11-22 12:21:18 -0800

    +spec feature view_question

--- spec/features/view_question_answer_spec.rb
+++ spec/features/view_question_answer_spec.rb
@@ -5,17 +5,17 @@ feature 'View question and answer', %q{
   As guest or user
   I want to be able to see question and answer
 }do
-  given(:user) { create(:user)}
-  given(:answer){ create(:answer)}
-  given(:question) do
-    create(:question){|question| question.answers.create(attributes_for(:answer))}
+  given!(:user) { create(:user)}
+  given!(:question) do
+    create(:question){|question| user.questions.create(attributes_for(:question))}
   end
+  given!(:answer){ create(:answer, question: question, user: user)}
   scenario 'Authenticated user try view question and answer' do
     sign_in(user)
     visit question_path(question)
     expect(page).to have_content question.body
     expect(page).to have_content answer.body
-end
+  end
   scenario 'NON-authenticated user try view question and answer' do
     visit question_path(question)
     expect(page).to have_content question.body
