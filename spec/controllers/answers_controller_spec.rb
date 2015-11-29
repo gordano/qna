0da86b79420a9b34110817f7f95053300cc05416
= <=>
2015-11-29 10:30:43 -0800

    +spec controller answer#makebest

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -25,7 +25,6 @@ RSpec.describe AnswersController, :type => :controller do
       end
     end
   end
-
   describe 'PATCH #update' do
     context 'valid attributes' do
       it 'assigned the requested answer to @answer' do
@@ -58,7 +57,6 @@ RSpec.describe AnswersController, :type => :controller do
       end
     end
   end
-
   describe 'PATCH #destroy' do
     sign_in_user
     before { question; answer}
@@ -68,6 +66,37 @@ RSpec.describe AnswersController, :type => :controller do
     it 'redirect to question path' do
       expect { delete :destroy, id: answer, question_id: question ,format: :js}.to change(Answer, :count).by(-1)
       expect(response).to render_template :destroy
+    end
+  end
+  describe "PATCH #make_best" do
+    sign_in_user
+    let!(:answer) {create(:answer, question: question, user: @user)}
+    let!(:question) {create(:question, user: @user)}
+    let!(:other_user) {create(:user)}
+    it "have_http_status(:ok)" do
+      patch :makebest, id: answer, question_id: question.id, format: :js
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "owner question check best answer" do
+      #patch :makebest, id: answer, question: question, format: :js
+      patch :makebest, id:answer,
+                     question_id: question.id,
+                     user_id: @user.id,
+                     answer: attributes_for(:answer),
+                     format: :js
+
+      expect(assigns(:answer).best).to be_truthy
+    end
+
+    it "user tried other question check best answer" do
+      sign_in(other_user)
+      patch :makebest, id:answer,
+                     question_id: question.id,
+                     user_id: @user.id,
+                     answer: attributes_for(:answer),
+                     format: :js
+      expect(assigns(:answer).best).to be_falsey
     end
   end
 end
